9e0b4bb1d343ef27e097f1ded8cbdb152d0f4392
Armin Ayatollahi <arminayat3@gmail.com>
2021-08-29 03:41:27 +0430

    feat(App): add Layout system to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Layout } from "antd";
+import styled from "styled-components";
+import Homepage from "./Pages/Homepage";
+
+const Content = styled(Layout.Content)`
+  padding: 10rem;
+  background-color: var(--bg);
+`;
+
 function App() {
   return (
-    <div>
-      cleaned, ready to start!
-    </div>
+    <Layout
+      style={{
+        height: "100%",
+        width: "100%",
+        minHeight: "100vh",
+        backgroundColor: "var(--bg)",
+      }}
+    >
+      {/* <Header /> */}
+      <Content>
+        <Homepage />
+      </Content>
+      {/* <Footer /> */}
+    </Layout>
   );
 }
 
