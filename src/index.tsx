130258f50f0d406d7d1c2222a2acd7be0e6ff939
Armin Ayatollahi <arminayat3@gmail.com>
2021-08-30 01:11:45 +0430

    feat(App): add react-query queryClient to app

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import "./Styles/index.scss";
 import "antd/dist/antd.css";
 import App from "./App";
 import { BalanceContextProvider } from "./ContextAPI/BalanceContex";
+import { QueryClientProvider } from "react-query";
+import queryClient from "./RESTservice/queryClient";
 
 ReactDOM.render(
   <React.StrictMode>
     <BalanceContextProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BalanceContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
