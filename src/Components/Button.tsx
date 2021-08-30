c3372af9985fa9f71c0becc2640753fa5a6a94f6
Armin Ayatollahi <arminayat3@gmail.com>
2021-08-30 13:28:55 +0430

    fix(Button): fix mobile not showing button content bug

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,3 +1,4 @@
+import { Button } from "antd";
 import styled from "styled-components";
 
 type ButtonProps = {
@@ -7,16 +8,19 @@ type ButtonProps = {
   height?: string;
   width?: string;
 };
-export default styled.button<ButtonProps>`
+export default styled(Button)<ButtonProps>`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
+  text-align: center;
   border-radius: 2rem;
   font-size: 2.8rem;
   font-weight: 400;
   height: ${(props) => props.height || "6rem"};
   width: ${(props) => props.width || "fit-content"};
-  padding-bottom: 0.2rem;
+  padding: 0;
+  /* padding-bottom: 0.2rem; */
+  border: 0;
   color: ${(props) => props.color || "rgba(217, 4, 41, 0.8)"};
   background: rgb(255, 255, 255);
   transition: 0.5s box-shadow ease;
